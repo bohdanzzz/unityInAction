9f92f63f6547ee823354d7bc870e6d49080cee78
Bohdan <bohdan.zemlianskyi@gmail.com>
2021-08-13 15:21:04 +0300

    ch1 and start ch2

---
+++ ch01x/ch01x/Assets/HelloThere.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloThere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()    
+    {
+        Debug.Log("aaaaaaaaa");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
