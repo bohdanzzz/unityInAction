c6c330de53d17ab33ca469c0a0bca798c2ad70c4
Bohdan <bohdan.zemlianskyi@gmail.com>
2021-08-18 00:44:36 +0300

    ch4.2.1

--- ch03x/ch03x/Assets/WanderingAI.cs
+++ ch03x/ch03x/Assets/WanderingAI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WanderingAI : MonoBehaviour
 {
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     private bool _alive;
@@ -17,13 +20,20 @@ public class WanderingAI : MonoBehaviour
         if(!_alive){
             return;
         }
-        
+
         transform.Translate(0, 0, speed * Time.deltaTime);
 
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        if(Physics.SphereCast(ray, 0.75f, out hit)){
-            if(hit.distance < obstacleRange){
+        if (Physics.SphereCast(ray, 0.75f, out hit)) {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<PlayerCharacter>()) {
+                if (_fireball == null) {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            } else if (hit.distance < obstacleRange){
                 float angle = Random.Range(-110, 110);
                 transform.Rotate(0, angle, 0);
             }
